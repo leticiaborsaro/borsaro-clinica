ee851bf7e7ea822ae5c042455cf65e2b72a86993
leticiaborsaro <leleborsaro@gmail.com>
2023-05-19 10:41:31 -0300

    aula dia 19/05

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function submit() {
+let email = document.getElementById('email').value;
+let password = document.getElementById('password').value;
+
+let resposta1 = document.getElementById('resposta').innerHTML
+let resposta2 = document.getElementById('resposta').innerHTML
+
+}
